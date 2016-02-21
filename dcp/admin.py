544e349e5329595bbd7d8598dcaf90cd4f920d19
santiago <santiago@pangea.org>
2016-02-21 23:35:10 +0100

    Customize PlaceAdmin (center, zoom & map size)

--- dcp/admin.py
+++ dcp/admin.py
@@ -3,4 +3,14 @@ from django.contrib.gis import admin
 from .models import Place
 
 
-admin.site.register(Place, admin.OSMGeoAdmin)
+class PlaceAdmin(admin.OSMGeoAdmin):
+    # coordinates on SRID=900913
+    # https://code.djangoproject.com/ticket/11094
+    default_lon = 236037
+    default_lat = 5068987
+    default_zoom = 7
+    map_height = 500
+    map_width = 800
+
+
+admin.site.register(Place, PlaceAdmin)
